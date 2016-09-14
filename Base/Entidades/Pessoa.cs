4a69dc6f9138009781abd0ccfcf1ac80822aec66
Rodrigo <rodrigo@andrade.com.br>
2016-09-14 14:05:20 -0300

    Carga Inicial projeto Base

---
+++ Base/Entidades/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public List<Telefone> Telefones { get; set; }
+
+        public Pessoa()
+        {
+            Telefones = new List<Telefone>();
+        }
+    }
+}
---
+++ Base/Entidades/Telefone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Base
+{
+    public class Telefone
+    {
+        public int Id { get; set; }
+        public int DDD { get; set; }
+        public int Numero { get; set; }
+        public TipoTelefone Tipo { get; set; }
+    }
+
+    public enum TipoTelefone
+    {
+        Fixo,
+        Celular
+    }
+}
